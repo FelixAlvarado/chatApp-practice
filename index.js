c08abfd07b7e3435b2a22a96481b6bdfc5b60f9e
FelixAlvarado <felixalvarado653@gmail.com>
2018-09-12 22:43:22 -0700

    set up socket in back and front end

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const socket = require('socket.io');
 
 
 
 
 
-app.listen(3000, () => console.log('listening on 3000'));
+let server = app.listen(3000, () => console.log('listening on 3000'));
 
 //using static files 
 app.use(express.static('public'));
+
+//socket setup 
+
+let io = socket(server);
+
+io.on('connection', (sock)=>{
+    console.log('made socket connection', sock.id);
+});
